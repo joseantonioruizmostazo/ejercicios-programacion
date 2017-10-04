6138c147ad52f66d11b1ef4555f1996c5e6761f9
Jose Antonio Ruiz Mostazo <jaruizmostazo@gmail.com>
2017-10-04 11:08:07 +0200

    Creando la rama de mi proyecto: Trainer Manager

---
+++ trainermanager/Trainermanager.java
@@ -0,0 +1,13 @@
+/**
+ * Proyecto Trainer Manager
+ */
+
+public class Trainermanager { // Clase principal
+	public static void main(String[] args) {
+
+		System.out.println("T R A I N E R  M A N A G E R");
+		System.out.println("1. Alta de producto");
+		System.out.println("2. Introducción");
+    
+  }
+}
