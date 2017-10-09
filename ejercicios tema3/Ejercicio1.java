b0a8aa04a443fdd0e77a8e55d1b4cc6c600ecc91
Jose Antonio Ruiz Mostazo <jaruizmostazo@gmail.com>
2017-10-09 22:03:09 +0200

    Añadido Ejercicio1 del tema 3

---
+++ ejercicios tema3/Ejercicio1.java
@@ -0,0 +1,29 @@
+/**
+ * Tema 3, ejercicio 1
+ * 
+ * @author José Ruiz
+ */
+
+public class Ejercicio1 {
+  public static void main(String[] args) {
+    String linea;
+    System.out.print("Por favor, introduce un número: ");
+    linea = System.console().readLine();
+    int primerNumero;
+    primerNumero = Integer.parseInt( linea );
+    
+    System.out.print("Por favor, introduce otro número: ");
+    linea = System.console().readLine();
+    int segundoNumero;
+    segundoNumero = Integer.parseInt( linea );
+    
+    int total;
+    total = primerNumero * segundoNumero;
+    
+    
+    System.out.print("El primer número introducido es " + primerNumero);
+    System.out.println(" y el segundo número es " + segundoNumero);
+    System.out.print("La multiplicación entre los dos números es " + total);
+    
+  }
+}
