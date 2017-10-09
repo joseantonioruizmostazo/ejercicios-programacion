416d75f035f1744a7b47ca3a248a80339fe97385
Jose Antonio Ruiz Mostazo <jaruizmostazo@gmail.com>
2017-10-09 22:34:38 +0200

    Añadido Ejercicios 1 y 2 del tema 3

---
+++ ejercicios tema3/Ejercicio2.java
@@ -0,0 +1,18 @@
+/**
+ * Tema 3, ejercicio 2
+ * 
+ * @author José Ruiz
+ */
+
+public class Ejercicio2 {
+  public static void main(String[] args) {
+    
+    System.out.print("Introduce una cantidad en euros que quieras convertir a pesetas: ");
+    double euros = Double.parseDouble(System.console().readLine());
+
+    int pesetas = (int) (euros * 166.386);
+    
+    System.out.print(euros + " euros son  " + pesetas + " pesetas.");
+    
+  }
+}
