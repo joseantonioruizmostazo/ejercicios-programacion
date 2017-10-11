84b083748c5fd4d894f0f5d8f9542bf4f890af27
Jose Antonio Ruiz Mostazo <jaruizmostazo@gmail.com>
2017-10-11 19:28:19 +0200

    Añadido Ejercicio 6 del tema 3

---
+++ ejercicios tema3/Ejercicio6.java
@@ -0,0 +1,23 @@
+/**
+ * Tema 3, ejercicio 6
+ * 
+ * @author José Ruiz
+ */
+ 
+import java.util.Scanner;
+
+public class Ejercicio6 {
+  public static void main(String[] args) {
+    
+    Scanner s = new Scanner(System.in);
+    
+    
+    System.out.println("Área de un triángulo");
+    System.out.print("Por favor, introduce la longitud de la base del triángulo expresada en cm: ");
+    double base = Double.parseDouble(s.nextLine());
+    System.out.print("Por favor, introduce la altura del triángulo expresada en cm: ");
+    double altura = Double.parseDouble(s.nextLine());
+    System.out.println("El área del triángulo es " + (base * altura) / 2 + " cm²");
+    
+  }
+}
