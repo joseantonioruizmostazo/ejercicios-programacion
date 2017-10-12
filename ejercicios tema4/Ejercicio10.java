dc9c368d446562175862667faab04b5bf5015539
Jose Antonio Ruiz Mostazo <jaruizmostazo@gmail.com>
2017-10-12 17:35:23 +0200

    Añadido Ejercicio 10 del tema 4

---
+++ ejercicios tema4/Ejercicio10.java
@@ -0,0 +1,115 @@
+/**
+ * Tema 4, ejercicio 10
+ * 
+ * @author José Ruiz
+ */
+ 
+import java.util.Scanner;
+
+public class Ejercicio10 {
+  
+  public static void main(String[] args) {
+    
+    Scanner s = new Scanner(System.in);
+    
+    String horoscopo = "";
+    
+    System.out.print("Por favor, introduce el día que naciste: ");
+    int dia = Integer.parseInt(s.nextLine());
+    
+    System.out.print("Por favor, introduce el mes (1-12): ");
+    int mes = Integer.parseInt(s.nextLine());
+    
+    switch(mes) {
+      case 1:
+        if (dia < 21) {
+          horoscopo = "capricornio";
+        } else {
+          horoscopo = "acuario";
+        }
+        break;
+      case 2:
+        if (dia < 20) {
+          horoscopo = "acuario";
+        } else {
+          horoscopo = "piscis";
+        }
+        break;
+      case 3:
+        if (dia < 21) {
+          horoscopo = "piscis";
+        } else {
+          horoscopo = "aries";
+        }
+        break;
+      case 4:
+        if (dia < 21) {
+          horoscopo = "aries";
+        } else {
+          horoscopo = "tauro";
+        }
+        break;
+      case 5:
+        if (dia < 20) {
+          horoscopo = "tauro";
+        } else {
+          horoscopo = "géminis";
+        }
+        break;
+      case 6:
+        if (dia < 22) {
+          horoscopo = "géminis";
+        } else {
+          horoscopo = "cáncer";
+        }
+        break;
+      case 7:
+        if (dia < 22) {
+          horoscopo = "cáncer";
+        } else {
+          horoscopo = "Leo";
+        }
+        break;
+      case 8:
+        if (dia < 24) {
+          horoscopo = "leo";
+        } else {
+          horoscopo = "virgo";
+        }
+        break;
+      case 9:
+        if (dia < 23) {
+          horoscopo = "virgo";
+        } else {
+          horoscopo = "libra";
+        }
+        break;
+      case 10:
+        if (dia < 23) {
+          horoscopo = "libra";
+        } else {
+          horoscopo = "escorpio";
+        }
+        break;
+      case 11:
+        if (dia < 23) {
+          horoscopo = "escorpio";
+        } else {
+          horoscopo = "sagitario";
+        }
+        break;
+      case 12:
+        if (dia < 21) {
+          horoscopo = "sagitario";
+        } else {
+          horoscopo = "capricornio";
+        }
+        break;
+      default:
+    }
+
+    System.out.print("Su horóscopo es " + horoscopo);
+
+  }
+}
+
