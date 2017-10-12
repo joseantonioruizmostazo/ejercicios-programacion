4dc79495dd396e3347361b1bd10174a07b5aca9f
Jose Antonio Ruiz Mostazo <jaruizmostazo@gmail.com>
2017-10-12 12:42:04 +0200

    Añadido Ejercicio 1 del tema 4

---
+++ ejercicios tema4/Ejercicio1.java
@@ -0,0 +1,46 @@
+/**
+ * Tema 4, ejercicio 1
+ * 
+ * @author José Ruiz
+ */
+ 
+import java.util.Scanner;
+
+public class Ejercicio1 {
+  
+  public static void main(String[] args) {
+    
+    Scanner s = new Scanner(System.in);
+    
+    String dia;
+    
+    System.out.print("Por favor, introduce un dia de la semana: ");
+    dia = (s.nextLine());
+    dia.toLowerCase();
+    
+    switch(dia) {
+      case "lunes":
+      System.out.println("A primera hora tienes programación");
+        break;
+      case "martes":
+      System.out.println("A primera hora tienes sistemas informáticos");
+        break;
+      case "miércoles":
+      System.out.println("A primera hora tienes programación");
+        break;
+      case "jueves":
+      System.out.println("A primera hora tienes entornos de desarrollo");
+        break;
+      case "viernes":
+      System.out.println("A primera hora tienes sistemas informáticos");
+        break;
+      case "sábado":
+      case "domingo":
+      System.out.println("Ese día no hay clase");
+        break;
+      default:
+        System.out.println("Lo siento, ese día no es correcto");
+        
+    }
+  }
+}
