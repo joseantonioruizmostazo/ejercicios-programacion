a59394b91881a6fe606e27c307209d6e41b1e59c
Jose Antonio Ruiz Mostazo <jaruizmostazo@gmail.com>
2017-10-12 18:03:59 +0200

    Añadido Ejercicio 11 del tema 4

---
+++ ejercicios tema4/Ejercicio11.java
@@ -0,0 +1,28 @@
+/**
+ * Tema 4, ejercicio 11
+ * 
+ * @author José Ruiz
+ */
+ 
+import java.util.Scanner;
+
+public class Ejercicio11 {
+  public static void main(String[] args) {
+    
+    Scanner s = new Scanner(System.in);
+    
+    
+    System.out.println("Cálculo de segundos que quedan hasta la media noche\n");
+    System.out.print("A continuación, introducirá una hora del día, primero introducirá la hora\n");
+    System.out.println("y luego los minutos");
+    System.out.print("Por favor, introduce hora: ");
+    int hora = Integer.parseInt(s.nextLine());
+    System.out.print("Ahora los minutos: ");
+    int minutos = Integer.parseInt(s.nextLine());
+    
+    int segundosTranscurridos = (hora * 3600) + (minutos * 60);
+    int segundosHastaMedianoche = (24 * 3600) - segundosTranscurridos;
+    
+    System.out.printf("Desde las %02d:%02d hasta la medianoche faltan %d segundos", hora, minutos, segundosHastaMedianoche);
+  }
+}
