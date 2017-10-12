266d31f82c6e5cdfc590fa99851239726a3bcb4b
Jose Antonio Ruiz Mostazo <jaruizmostazo@gmail.com>
2017-10-12 15:10:00 +0200

    Añadido Ejercicio 6 del tema 4

---
+++ ejercicios tema4/Ejercicio6.java
@@ -0,0 +1,24 @@
+/**
+ * Tema 4, ejercicio 6
+ * 
+ * @author José Ruiz
+ */
+ 
+import java.util.Scanner;
+
+public class Ejercicio6 {
+  
+  static final double g = 9.81;
+  public static void main(String[] args) {
+    
+    Scanner s = new Scanner(System.in);
+    
+    
+    System.out.println("Tiempo en caer un objeto desde una altura\n");
+    System.out.print("Por favor, introduce la altura (en metros) desde la que cae el objeto: ");
+    double h = Double.parseDouble(s.nextLine());
+    double t = ((2 * h) / g) * ((2 * h) / g);
+    System.out.println("El tiempo que tardará en caer será de " + t + " segundos");
+    
+  }
+}
