21782fe0bf31e3f3e8b0ec7cb39ca5951d23648b
Jose Antonio Ruiz Mostazo <jaruizmostazo@gmail.com>
2017-10-22 17:19:36 +0200

    Añadido ejercicio 9 del tema 5

---
+++ ejercicios tema5/Ejercicio9.java
@@ -0,0 +1,34 @@
+/**
+ * Tema 5, ejercicio 9
+ * 
+ * Realiza un programa que nos diga cuántos dígitos tiene un número introducido
+ * por teclado.
+ * 
+ * @author José Ruiz
+ */
+ 
+ import java.util.Scanner;
+
+public class Ejercicio9 {
+  public static void main(String[] args) {
+    
+    Scanner s = new Scanner(System.in);
+    
+    int numeroDeDigitos = 1;
+    int n;
+    int numeroIntroducido;
+    
+    System.out.println("Por favor, introduzca un número y te diré cuantos dígitos tiene.\n");
+    numeroIntroducido = Integer.parseInt(s.nextLine());
+    
+    n = numeroIntroducido;
+    
+    while (n > 10) {
+      n /= 10;
+      numeroDeDigitos++;
+    }
+    
+    System.out.println(numeroIntroducido + " tiene " + numeroDeDigitos + " dígito/s.");
+
+  }
+}
