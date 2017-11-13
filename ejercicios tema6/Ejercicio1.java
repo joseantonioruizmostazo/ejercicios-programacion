ee62fc702c3e1d76e05d6987638f7e3659d63003
Jose Antonio Ruiz Mostazo <jaruizmostazo@gmail.com>
2017-11-13 10:23:34 +0100

    añadido ejercicio 1 del tema 6

---
+++ ejercicios tema6/Ejercicio1.java
@@ -0,0 +1,30 @@
+/**
+ * Tema 6, ejercicio 1
+ * 
+ * Escribe un programa que muestre la tirada de tres dados. Se debe mostrar
+ * también la suma total (los puntos que suman entre los tres dados).
+ * 
+ * @author José Ruiz
+ */
+ 
+ import java.util.Scanner;
+
+public class Ejercicio1 {
+  public static void main(String[] args) {
+    
+    Scanner s = new Scanner(System.in);
+    int dado;
+    int suma = 0;
+    
+    System.out.print("A continuación se mostrará la tirada de tres dados y se sumarán todos los puntos de los tres dados.\n");
+    
+    for (int i = 1; i <= 3; i++) {
+      dado = ((int)(Math.random() * 6) + 1);
+      System.out.println("Dado " + i + ": " + dado);
+      suma += dado;
+    }
+    System.out.println();
+    System.out.print("La suma de los dados es: " + suma);
+    
+  }
+}
