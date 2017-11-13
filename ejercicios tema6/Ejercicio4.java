72efccff8cadf103bd6760eb1b0bb01acd847ef4
Jose Antonio Ruiz Mostazo <jaruizmostazo@gmail.com>
2017-11-13 23:01:24 +0100

    Añadido ejercicio 4 del tema 6

---
+++ ejercicios tema6/Ejercicio4.java
@@ -0,0 +1,23 @@
+/**
+ * Tema 6, ejercicio 4
+ * 
+ * Muestra 20 números enteros aleatorios entre 0 y 10 (ambos incluidos)
+ * separados por espacios.
+ * 
+ * @author José Ruiz
+ */
+ 
+ import java.util.Scanner;
+
+public class Ejercicio4 {
+  public static void main(String[] args) {
+    
+    Scanner s = new Scanner(System.in);
+    
+    System.out.print("A continuación se mostrarán 20 números enteros aleatorios entre 0 y 10 (ambos incluidos)\n");
+    
+    for (int i = 1; i <= 20; i++) {
+      System.out.print((int)(Math.random() * 11) + " ");
+    }
+  }
+}
