b641465b8a50e20d4bdd900f40cbad689a2ed70b
joseantonioruizmostazo <jaruizmostazo@gmail.com>
2017-11-22 11:36:55 +0100

    Añadido ejercicio 1 tema 7

---
+++ ejercicios tema7/Ejercicio1.java
@@ -0,0 +1,31 @@
+/*
+ * Define un array de 12 números enteros con nombre num y asigna los valores
+ * según la tabla que se muestra a continuación. Muestra el contenido de todos
+ * los elementos del array. ¿Qué sucede con los valores de los elementos que no
+ * han sido inicializados?
+ * 
+ * @author José Ruiz
+ * 
+ */
+
+
+
+public class Ejercicio1 {
+  public static void main(String[] args) {
+    
+    System.out.print("A continuación se mostrará el contenido de los elementos de un array\n");
+    System.out.println("con 12 números enteros.\n");
+    
+    int[] num = new int[12];
+    num[0] = 39;
+    num[1] = -2;
+    num[4] = 0;
+    num[6] = 14;
+    num[8] = 5;
+    num[9] = 120;
+    
+    for (int i = 0; i < 12; i++){
+    System.out.println(num[i]);
+    }
+  }
+}
